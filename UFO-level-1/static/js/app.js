0fb39e527f2a13b3de7c9d29ce4d23b3844fdeda
Esaa Yamini <63874540+eyamini@users.noreply.github.com>
2020-08-22 14:37:24 -0500

    Update app.js

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -2,3 +2,44 @@
 var tableData = data;
 
 // YOUR CODE HERE!
+var tbody = d3.select("tbody");
+console.log(tableData);
+
+// Populate sighting column values
+tableData.forEach(function(sightings) {
+    console.log(sightings);
+    var row = tbody.append("tr");
+
+    // HTML Console Log sightings
+    Object.entries(sightings).forEach(function([key, value]) {
+      console.log(key, value);
+      var cell = row.append("td");
+      cell.text(value);
+    });
+  });
+
+
+// Submit Button
+var submit = d3.select("#filter-btn");
+submit.on("click", function() {
+    //Reset Value
+    tbody.html("");
+    //Get date/time
+    var datetime = d3.select("#datetime");
+    var datetime = datetime.property("value");
+    console.log(datetime);
+    // Filter the data
+    var filterdata = tableData.filter(sighting => sighting.datetime === datetime);
+    console.log(filterdata);
+    //Display the data
+    filterdata.forEach(function(reporting) {
+    console.log(reporting);
+    var row = tbody.append("tr");
+
+    Object.entries(reporting).forEach(function([key, value]) {
+        console.log(key, value);
+        var cell = row.append("td");
+        cell.text(value);
+    });
+});
+});
